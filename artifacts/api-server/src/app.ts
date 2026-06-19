dba9c66708ccfb2137b537bae8f0b2db67cf9dd2
Mmaduabuchi Onah <ocbuchi@gmail.com>
2026-06-19 21:33:05 +0100

    Removed pino as it is not compatible with cloudflare workers

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,5 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
 import { clerkMiddleware } from "@clerk/express";
 import { CLERK_PROXY_PATH, clerkProxyMiddleware } from "./middlewares/clerkProxyMiddleware";
 import router from "./routes";
@@ -9,25 +8,23 @@ import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  }),
-);
+// Custom request logging middleware (replaces pino-http)
+app.use((req, res, next) => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    logger.info({
+      method: req.method,
+      url: req.url?.split("?")[0],
+      statusCode: res.statusCode,
+      duration: `${duration}ms`,
+      // You can add more fields if needed
+    });
+  });
+
+  next();
+});
 
 app.use(CLERK_PROXY_PATH, clerkProxyMiddleware());
 
